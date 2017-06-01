b715f2d0fbc4bfe961e6b60c07fe7ff9d94ba0e6
Rob Cannon <robert.cannon@turner.com>
2017-06-01 13:45:19 -0400

    Added Requests and Config components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    activeMenuId: string;
+    activeMenuId = 'requests-menu';
     themesVisible = false;
     mobileMenuActive = false;
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { InputTextModule, ButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { RequestsComponent } from './requests/requests.component';
+import { ConfigComponent } from './config/config.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    InputTextModule,
-    ButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RequestsComponent,
+        ConfigComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpModule,
+        InputTextModule,
+        ButtonModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                component: RequestsComponent
+            },
+            {
+                path: 'config',
+                component: ConfigComponent
+            }
+
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
